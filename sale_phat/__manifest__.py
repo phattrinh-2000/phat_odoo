d33158cd242a97f0a7c43d27a6b1ab4beaa516d6
phattrinh-2000 <phat080720@gmail.com>
2024-07-22 18:45:07 +0700

    update

---
+++ sale_phat/__manifest__.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'sinh_vien',
+    'version': "1.01",
+    'author': "Sinhvien",
+    'category': 'Sales',
+    'sequence': 100,
+    'summary': 'sinhviennamnhat',
+    'description': "",
+    'depends': ['base'],
+    'data': [
+        'security/ir.model.access.csv',
+        'views/phat_de_thuong_cute.xml',
+        'views/sinh_vien_stage_views.xml',
+    ],
+    'demo': [],
+    'installable': True,
+    'application': True,
+    'auto_install': False,
+    'website': 'https://www.giant.com',
+    'license': 'LGPL-3',
+    'images': ['static/description/icon.png'],
+}
